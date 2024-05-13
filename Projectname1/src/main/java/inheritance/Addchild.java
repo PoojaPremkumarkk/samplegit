5679743a32e2aa65357fc4a65fff590c1ae6618b
pooja <pooja@LAPTOP-FD43FT9U>
2024-05-13 20:17:13 +0500

    today's program

---
+++ Projectname1/src/main/java/inheritance/Addchild.java
@@ -0,0 +1,35 @@
+package inheritance;
+
+public class Addchild extends Addparent{
+	static int d;
+	public boolean div()
+	{
+		
+		d=super.add(29, 76);
+		return d%10==0;
+	
+		
+	}
+	public void method2()
+	{
+		if(d%10==0)
+		{
+			System.out.println(" the number is divisible by 10");
+		}
+		else
+		{
+			System.out.println("the number is not divisible by 10");
+		}
+	}
+
+	public static void main(String[] args) {
+		Addchild obj=new Addchild();
+		obj.div();
+		obj.add(3, 9);
+		obj.method2();
+		System.out.println("The result of addition = " +d);
+		
+
+	}
+
+}
---
+++ Projectname1/src/main/java/inheritance/Addparent.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Addparent {
+	int c;
+	
+	public int add(int a,int b)
+	{
+		c=a+b;
+		return c;
+	}
+
+}
